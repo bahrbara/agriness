0f338ed950d6b6234d1c1cf96e1a9f9f12732022
Bárbara Cristina Blank Garibaldi <barbaracbgaribaldi@gmail.com>
2020-06-07 22:40:57 -0300

    initial commit

---
+++ src/main/java/com/test/agriness/AgrinessApplication.java
@@ -0,0 +1,13 @@
+package com.test.agriness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrinessApplication.class, args);
+	}
+
+}
